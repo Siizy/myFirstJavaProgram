8a33d1ff9273e79528aa54db46b359c1a767d871
mamta <mamta@Mamta_Gupta.ht.home>
2024-12-09 12:51:47 -0500

    added a small change in Bajaj class

--- src/encap/Bajaj.java
+++ src/encap/Bajaj.java
@@ -6,6 +6,8 @@ public class Bajaj extends MotorBike{
 	public void applyBrake() {
 		System.out.println("");
 		
+	  System.out.println("I am adding a sentence");
+		
 	}
 	
 	public static void main(String[] args) {
